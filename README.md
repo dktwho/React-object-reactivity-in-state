2daf2d28c7b3d40ef86cb2f7b84b32122e620f2b
Alexander Korobov <dktwho@mail.ru>
2023-01-12 16:48:36 +0300

    first commit

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# Содержание
+
+Реактивность объекта, есть три кнопки, каждая меняет свое собственной значение в объекте
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [obj, setObj] = useState({
+    prop1: 'value1',
+    prop2: 'value2',
+    prop3: 'value3',
+  })
+
+  function changeProp1() {
+    setObj({...obj, ...{prop1: 'value1NEW'}})
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span>{obj.prop1}</span> <button onClick={changeProp1}>change 1 </button> <br />
+      <span>{obj.prop2}</span> <button onClick={() => setObj({...obj, ...{prop2: 'value2NEW'}})}>change 2</button><br />
+      <span>{obj.prop3}</span> <button onClick={() => setObj({...obj, ...{prop3: 'value3NEW'}})}>change 3</button><br />
+
     </div>
   );
 }
